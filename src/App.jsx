d85daee6f3991734b07f493ca8304aff7d73908e
izniy <ngyinzi1303@gmail.com>
2025-02-08 16:52:00 +0800

    Trigger Vercel redeployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Card = ({ title }) => {
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]);
-
-    return (
-        <div className="card" onClick={() => setCount(count + 1)}>
-            <h2>{title} <br/> {count || null}</h2>
-
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? "❤️" : "🤍"}
-            </button>
-        </div>
-    )
-}
+import React from 'react'
 
 const App = () => {
-    const [hasLiked, setHasLiked] = useState(false);
     return (
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true} />
-            <Card title="Avatar" />
-            <Card title="The Lion King" />
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
-
 export default App
